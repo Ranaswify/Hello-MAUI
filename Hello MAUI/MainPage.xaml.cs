7ab9d68953da417096bca3ea14d5697b91dfbb3d
Rana Muhammad <Ranaswify@hotmail.com>
2022-01-02 14:55:46 +0200

    Add project files.

---
+++ Hello MAUI/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Hello_MAUI
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnButtonClicked(object sender, EventArgs e)
+		{
+			App.Current.MainPage.DisplayAlert("", "Hello World.", "Okay");
+
+		}
+	}
+}
---
+++ Hello MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Hello_MAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
